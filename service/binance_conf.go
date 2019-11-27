7187f09fd5b22faf10581025d9071f2573f0d9bd
Justin <zhou78620051@126.com>
2019-11-27 10:38:05 +0800

    fix: go111module switch, conf variable defination

--- service/binance_conf.go
+++ service/binance_conf.go
@@ -27,72 +27,72 @@ type BinanceConf struct {
 }
 
 // Init 初始化配置
-func (bc *BinanceConf) Init(filename string) {
-	bc.Filename = filename
+func (conf *BinanceConf) Init(filename string) {
+	conf.Filename = filename
 
-	bc.ReadWatchConf()
+	conf.ReadWatchConf()
 
 	go func() {
 		ticker := time.NewTicker(time.Second * 5)
 		for range ticker.C {
-			bc.ReadWatchConf()
+			conf.ReadWatchConf()
 		}
 	}()
 }
 
 // FormatSymbol 格式化 Symbol
-func (bc *BinanceConf) FormatSymbol(symbol string) string {
+func (conf *BinanceConf) FormatSymbol(symbol string) string {
 	return strings.ToUpper(strings.Replace(symbol, "_", "", 1))
 }
 
 // FormatSymbols 格式化 Symbols
-func (bc *BinanceConf) FormatSymbols(symbols []string) []string {
+func (conf *BinanceConf) FormatSymbols(symbols []string) []string {
 	var n = []string{}
 	for _, symbol := range symbols {
-		n = append(n, bc.FormatSymbol(symbol))
+		n = append(n, conf.FormatSymbol(symbol))
 	}
 	return n
 }
 
 // ReadWatchConf 读取监听盘口价格配置
-func (bc *BinanceConf) ReadWatchConf() (err error) {
+func (conf *BinanceConf) ReadWatchConf() (err error) {
 	iniParser := utils.IniParser{}
-	if err := iniParser.Load(bc.Filename); err != nil {
-		fmt.Printf("try load config file[%s] error[%s]\n", bc.Filename, err.Error())
+	if err := iniParser.Load(conf.Filename); err != nil {
+		fmt.Printf("try load config file[%s] error[%s]\n", conf.Filename, err.Error())
 		return err
 	}
 
-	bc.Symbol = iniParser.GetString("binance", "symbol")
-	bc.Depth = iniParser.GetString("binance", "depth")
-	bc.Level = int(iniParser.GetInt32("binance", "level"))
-	bc.Interval = iniParser.GetString("binance", "interval")
-	bc.Symbols = bc.FormatSymbols(Uniq(strings.Split(bc.Symbol, ",")))
+	conf.Symbol = iniParser.GetString("binance", "symbol")
+	conf.Depth = iniParser.GetString("binance", "depth")
+	conf.Level = int(iniParser.GetInt32("binance", "level"))
+	conf.Interval = iniParser.GetString("binance", "interval")
+	conf.Symbols = conf.FormatSymbols(Uniq(strings.Split(conf.Symbol, ",")))
 
-	if bc.Symbol != bc.histSymbol ||
-		bc.Depth != bc.histDepth ||
-		bc.Level != bc.histLevel ||
-		bc.Interval != bc.histInterval {
-		bc.isChanged = true
+	if conf.Symbol != conf.histSymbol ||
+		conf.Depth != conf.histDepth ||
+		conf.Level != conf.histLevel ||
+		conf.Interval != conf.histInterval {
+		conf.isChanged = true
 	}
 
-	bc.histSymbol = bc.Symbol
-	bc.histDepth = bc.Depth
-	bc.histLevel = bc.Level
-	bc.histInterval = bc.Interval
+	conf.histSymbol = conf.Symbol
+	conf.histDepth = conf.Depth
+	conf.histLevel = conf.Level
+	conf.histInterval = conf.Interval
 
-	if bc.isChanged == true {
-		fmt.Println("binance conf hot reload", bc)
+	if conf.isChanged == true {
+		fmt.Println("binance conf hot reload", conf)
 
-		for _, observer := range bc.observers {
+		for _, observer := range conf.observers {
 			observer.Notify()
 		}
-		bc.isChanged = false
+		conf.isChanged = false
 	}
 
 	return nil
 }
 
 // AddObserver 注册观察者
-func (bc *BinanceConf) AddObserver(observer Observer) {
-	bc.observers = append(bc.observers, observer)
+func (conf *BinanceConf) AddObserver(observer Observer) {
+	conf.observers = append(conf.observers, observer)
 }
--- service/okex_conf.go
+++ service/okex_conf.go
@@ -21,79 +21,78 @@ type OKExConf struct {
 	histDepth    string
 	histLevel    int
 	histInterval string
-	histSymbols  []string
 
 	isChanged bool
 	observers []Observer
 }
 
 // Init 初始化配置
-func (oc *OKExConf) Init(filename string) {
-	oc.Filename = filename
+func (conf *OKExConf) Init(filename string) {
+	conf.Filename = filename
 
-	oc.ReadWatchConf()
+	conf.ReadWatchConf()
 
 	go func() {
 		ticker := time.NewTicker(time.Second * 5)
 		for range ticker.C {
-			oc.ReadWatchConf()
+			conf.ReadWatchConf()
 		}
 	}()
 }
 
 // FormatSymbol 格式化 Symbol
-func (oc *OKExConf) FormatSymbol(symbol string) string {
+func (conf *OKExConf) FormatSymbol(symbol string) string {
 	return strings.ToUpper(strings.Replace(symbol, "_", "-", 1))
 }
 
 // FormatSymbols 格式化 Symbols
-func (oc *OKExConf) FormatSymbols(symbols []string) []string {
+func (conf *OKExConf) FormatSymbols(symbols []string) []string {
 	var n = []string{}
 	for _, symbol := range symbols {
-		n = append(n, oc.FormatSymbol(symbol))
+		n = append(n, conf.FormatSymbol(symbol))
 	}
 	return n
 }
 
 // ReadWatchConf 读取监听盘口价格配置
-func (oc *OKExConf) ReadWatchConf() (err error) {
+func (conf *OKExConf) ReadWatchConf() (err error) {
 	iniParser := utils.IniParser{}
-	if err := iniParser.Load(oc.Filename); err != nil {
-		fmt.Printf("try load config file[%s] error[%s]\n", oc.Filename, err.Error())
+	if err := iniParser.Load(conf.Filename); err != nil {
+		fmt.Printf("try load config file[%s] error[%s]\n", conf.Filename, err.Error())
 		return err
 	}
 
-	oc.Symbol = iniParser.GetString("okex", "symbol")
-	oc.Depth = iniParser.GetString("okex", "depth")
-	oc.Level = int(iniParser.GetInt32("okex", "level"))
-	oc.Interval = iniParser.GetString("okex", "interval")
-	oc.Symbols = oc.FormatSymbols(Uniq(strings.Split(oc.Symbol, ",")))
+	conf.Symbol = iniParser.GetString("okex", "symbol")
+	conf.Depth = iniParser.GetString("okex", "depth")
+	conf.Level = int(iniParser.GetInt32("okex", "level"))
+	conf.Interval = iniParser.GetString("okex", "interval")
+	conf.Symbols = conf.FormatSymbols(Uniq(strings.Split(conf.Symbol, ",")))
 
-	if oc.Symbol != oc.histSymbol ||
-		oc.Depth != oc.histDepth ||
-		oc.Level != oc.histLevel ||
-		oc.Interval != oc.histInterval {
-		oc.isChanged = true
+	if conf.Symbol != conf.histSymbol ||
+		conf.Depth != conf.histDepth ||
+		conf.Level != conf.histLevel ||
+		conf.Interval != conf.histInterval {
+		conf.isChanged = true
 	}
 
-	oc.histSymbol = oc.Symbol
-	oc.histDepth = oc.Depth
-	oc.histLevel = oc.Level
-	oc.histInterval = oc.Interval
+	conf.histSymbol = conf.Symbol
+	conf.histDepth = conf.Depth
+	conf.histLevel = conf.Level
+	conf.histInterval = conf.Interval
 
-	if oc.isChanged == true {
-		fmt.Println("okex conf hot reload", oc)
+	if conf.isChanged == true {
+		fmt.Println("okex conf hot reload", conf)
 
-		for _, observer := range oc.observers {
+		for _, observer := range conf.observers {
 			observer.Notify()
 		}
-		oc.isChanged = false
+		conf.isChanged = false
 	}
 
 	return nil
 }
 
 // AddObserver 注册观察者
-func (oc *OKExConf) AddObserver(observer Observer) {
-	oc.observers = append(oc.observers, observer)
+func (conf *OKExConf) AddObserver(observer Observer) {
+	conf.observers = append(conf.observers, observer)
 }
